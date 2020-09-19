0231d097006be8617dc8489c241d3b68d7d9ca8e
AGW <agiraldow@gmail.com>
2020-09-19 19:21:21 -0400

    Add poem css

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import styled from "styled-components"
 
 import Header from "./header"
 import { device } from "./device"
+import "./layout.css"
 
 const GlobalStyle = createGlobalStyle`
   body {
